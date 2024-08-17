f3518196ea104f2cabe018cca814a61a57a144af
RahulKoju <blcraz75@gmail.com>
2024-08-17 22:28:07 +0545

    Add: User model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+});
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  chat: [chatSchema],
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
