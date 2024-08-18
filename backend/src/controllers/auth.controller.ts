3a36defc007dabd5772b46e3f3817f491c988300
RahulKoju <blcraz75@gmail.com>
2024-08-18 12:28:17 +0545

    Fix & Add: sign up api and sign in api route

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,17 +1,70 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/user.model";
 import bcrypt from "bcrypt";
+import { errorHandler } from "../utils/error.utils";
+
+// Handle Sign Up
 export const handleSignUp = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Promise<void> => {
   try {
     const { name, email, password } = req.body;
+
+    // Validate request data
+    if (
+      !name ||
+      !email ||
+      !password ||
+      name.trim() === "" ||
+      email.trim() === "" ||
+      password.trim() === ""
+    ) {
+      return next(errorHandler(400, "All fields are required."));
+    }
+
+    // Check if the user already exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return next(errorHandler(409, "User with given email already exists!"));
+    }
+
+    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ name, email, password: hashedPassword });
+
+    // Create and save the new user
+    user = new User({ name, email, password: hashedPassword });
     await user.save();
-    return res.status(200).json({ message: "Sign Up successful" });
+
+    res.status(200).json({ message: "Sign Up successful" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Handle Sign In
+export const handleSignIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(401, "User not found"));
+    }
+
+    // Compare passwords
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return next(errorHandler(403, "Incorrect password"));
+    }
+
+    res.status(200).json({ message: "Sign In successful" });
   } catch (error) {
     next(error);
   }
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { handleGetAllUsers } from "../controllers/user.controller";
-import { handleSignUp } from "../controllers/auth.controller";
+import { handleSignIn, handleSignUp } from "../controllers/auth.controller";
 import validation from "../middlewares/validation.middleware";
-import { signUpSchema } from "../utils/validation.util";
+import { signInSchema, signUpSchema } from "../utils/validation.util";
 
 const router = Router();
 
 router.get("/getallusers", handleGetAllUsers);
 router.post("/signup", validation(signUpSchema), handleSignUp);
+router.post("/signin", validation(signInSchema), handleSignIn);
 
 export default router;
