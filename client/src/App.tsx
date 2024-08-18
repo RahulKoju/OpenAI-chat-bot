d71ab4e55ee4e5be9a747f30df03fa77455f8f35
RahulKoju <blcraz75@gmail.com>
2024-08-18 16:49:46 +0545

    Add: Initialized all component and pages for UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/signUp";
+import Chat from "./pages/Chat";
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createTheme, ThemeProvider } from "@mui/material";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto Slab,serif",
+    allVariants: { color: "white" },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>
+);
