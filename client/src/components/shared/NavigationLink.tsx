ad78424296beb55970bae5ce75ecb15aaaf4e2a2
RahulKoju <blcraz75@gmail.com>
2024-08-19 15:33:00 +0545

    Add: Navigation link component

---
+++ client/src/components/shared/NavigationLink.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+type Props = {
+  to: string;
+  bg: string;
+  text: string;
+  textColor: string;
+  onClick?: () => Promise<void>;
+};
+export const NavigationLink = (props: Props) => {
+  return (
+    <Link
+      className="font-semibold uppercase mx-2 px-4 py-3 rounded-xl"
+      to={props.to}
+      style={{ background: props.bg, color: props.textColor }}
+    >
+      {props.text}
+    </Link>
+  );
+};
