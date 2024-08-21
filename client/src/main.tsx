493223c2ebb66def7dec523b0a2b6259730a747f
RahulKoju <blcraz75@gmail.com>
2024-08-21 14:01:26 +0545

    Add: Toaster which gives notification at the top

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.tsx";
-
+import {Toaster} from "react-hot-toast"
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:8000/api";
+axios.defaults.withCredentials = true;
 const theme = createTheme({
   typography: {
     fontFamily: "Roboto Slab,serif",
@@ -14,6 +17,7 @@ const theme = createTheme({
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
     <ThemeProvider theme={theme}>
+      <Toaster position="top-center"/>
       <App />
     </ThemeProvider>
   </AuthProvider>
