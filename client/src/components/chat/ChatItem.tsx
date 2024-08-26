a4de2c19d76aaa2f49bcee1c07cf98d1fdcaf0a7
RahulKoju <blcraz75@gmail.com>
2024-08-26 20:14:35 +0545

    Add: Display code blocks in chat page

--- client/src/components/chat/ChatItem.tsx
+++ client/src/components/chat/ChatItem.tsx
@@ -1,5 +1,30 @@
 import { Avatar, Box, Typography } from "@mui/material";
 import { useAuth } from "../../helpers/useAuth";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coldarkDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+function extractCodeFromString(message: string) {
+  if (message.includes("```")) {
+    const blocks = message.split("```");
+    return blocks;
+  }
+}
+
+function isCodeBlock(str: string) {
+  if (
+    str.includes("=") ||
+    str.includes(";") ||
+    str.includes("[") ||
+    str.includes("]") ||
+    str.includes("{") ||
+    str.includes("}") ||
+    str.includes("#") ||
+    str.includes("//")
+  ) {
+    return true;
+  }
+  return false;
+}
 
 export const ChatItem = ({
   content,
@@ -9,23 +34,46 @@ export const ChatItem = ({
   role: "user" | "assistant";
 }) => {
   const auth = useAuth();
+  const messageBlocks = extractCodeFromString(content);
   return role === "assistant" ? (
     <Box sx={{ display: "flex", p: 2, bgcolor: "#004d5612", my: 2, gap: 2 }}>
       <Avatar sx={{ ml: "0" }}>
         <img src="open_ai.png" alt="openai" width={"30px"} />
       </Avatar>
       <Box>
-        <Typography fontSize={"20px"}>{content}</Typography>
+        {!messageBlocks && <Typography fontSize={"20px"}>{content}</Typography>}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((block) =>
+            isCodeBlock(block) ? (
+              <SyntaxHighlighter style={coldarkDark} language="javascript">
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography fontSize={"20px"}>{content}</Typography>
+            )
+          )}
       </Box>
     </Box>
   ) : (
-    <Box sx={{ display: "flex", p: 2, bgcolor: "#004d56", gap: 2 }}>
+    <Box sx={{ display: "flex", p: 2, bgcolor: "#004d56", gap: 2, my: 2 }}>
       <Avatar sx={{ ml: "0", bgcolor: "black", color: "white" }}>
         {auth?.user?.name[0]}
         {auth?.user?.name.split(" ")[1][0]}
       </Avatar>
       <Box>
-        <Typography fontSize={"20px"}>{content}</Typography>
+        {!messageBlocks && <Typography fontSize={"20px"}>{content}</Typography>}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((block) =>
+            isCodeBlock(block) ? (
+              <SyntaxHighlighter style={coldarkDark} language="javascript">
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography fontSize={"20px"}>{content}</Typography>
+            )
+          )}
       </Box>
     </Box>
   );
