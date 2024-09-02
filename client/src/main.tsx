ef8c2075f8f611efa377da96d1d7fff60a76fec3
RahulKoju <blcraz75@gmail.com>
2024-09-02 19:49:06 +0545

    Fix: Backend url

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import {Toaster} from "react-hot-toast"
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8000/api";
+axios.defaults.baseURL = import.meta.env.BASE_URL;
 axios.defaults.withCredentials = true;
 const theme = createTheme({
   typography: {
